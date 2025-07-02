05ced6529367200018ea63f567868689ea7c38d2
Anup Gupta <anuup22.cs@gmail.com>
2025-07-02 08:32:18 +0530

    Add MessageChannel enum for SMS, WhatsApp, and Facebook

---
+++ src/main/java/com/nonstop/twilio/enums/MessageChannel.java
@@ -0,0 +1,23 @@
+package com.nonstop.twilio.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MessageChannel {
+    SMS("sms"),
+    WHATSAPP("whatsapp"),
+    FACEBOOK("facebook");
+    
+    private final String value;
+    
+    public static MessageChannel fromString(String channel) {
+        for (MessageChannel c : MessageChannel.values()) {
+            if (c.value.equalsIgnoreCase(channel)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException("Unknown channel: " + channel);
+    }
+}
