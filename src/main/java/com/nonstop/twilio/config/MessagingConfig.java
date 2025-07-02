dc34292ef8102c9ef382dbe78a20adb620ee28b3
Anup Gupta <anuup22.cs@gmail.com>
2025-07-02 09:04:33 +0530

    Refactor messaging configuration

--- src/main/java/com/nonstop/twilio/config/MessagingConfig.java
+++ src/main/java/com/nonstop/twilio/config/MessagingConfig.java
@@ -12,10 +12,10 @@ import com.twilio.Twilio;
 @Configuration
 public class MessagingConfig {
     
-    @Value("${messaging.twilio.account-sid}")
+    @Value("${twilio.account-sid}")
     private String twilioAccountSid;
     
-    @Value("${messaging.twilio.auth-token}")
+    @Value("${twilio.auth-token}")
     private String twilioAuthToken;
     
     @PostConstruct
